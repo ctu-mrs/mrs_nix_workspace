e37a9a613b2ee70394ba155241f3efaeba357b37
Tomas Baca <Tomas Baca>
2026-05-31 17:39:11 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -5,9 +5,11 @@
     nix-mrs-overlay.url = "github:ctu-mrs/nix-mrs-overlay/master";
 
     nixpkgs.follows = "nix-mrs-overlay/nixpkgs";
+
+    nixgl.url = "github:nix-community/nixGL";
   };
 
-  outputs = { self, nix-ros-overlay, nix-mrs-overlay, nixpkgs }:
+  outputs = { self, nix-ros-overlay, nix-mrs-overlay, nixgl, nixpkgs }:
     nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (system:
       let
 
@@ -16,6 +18,7 @@
           overlays = [
             nix-ros-overlay.overlays.default
             nix-mrs-overlay.overlays.default
+            nixgl.overlay
           ];
         };
 
@@ -27,13 +30,21 @@
 
           name = "MRS shell";
 
+          env.RMW_IMPLEMENTATION="rmw_cyclonedds_cpp";
+
+          env.TERMINFO_DIRS = "${pkgs.rxvt-unicode-unwrapped.terminfo}/share/terminfo:/usr/share/terminfo:/lib/terminfo";
+
           packages = [
+
             pkgs.colcon
             pkgs.tmux
             pkgs.tmuxinator
 
             ros.ros-core
             ros.rviz2
+            ros.rmw-zenoh-cpp
+            ros.rmw-cyclonedds-cpp
+            ros.rclpy
 
             mrs.mrs_multirotor_simulator
             mrs.mrs_uav_managers
@@ -43,6 +54,35 @@
             mrs.mrs_uav_core
             
           ];
+
+          shellHook = ''
+            echo "🔧 Welcome to the MRS devenv environment!"
+
+            # ROS autocomplete
+            eval "$(register-python-argcomplete ros2)"
+            eval "$(register-python-argcomplete colcon)"
+
+            # for nixGL graphics
+            export QT_QPA_PLATFORM=xcb
+            export QT_X11_NO_MITSHM=1
+
+            # for nvidia+integrated hybrid systems
+            export __NV_PRIME_RENDER_OFFLOAD=1
+            export __GLX_VENDOR_LIBRARY_NAME=nvidia
+            export __VK_LAYER_NV_optimus=NVIDIA_only
+
+            if command -v nixGL &> /dev/null; then
+              echo "☢️ WARNING: Globally injecting host graphics drivers into shell..."
+
+              export LD_LIBRARY_PATH=$(nixGL printenv LD_LIBRARY_PATH):$LD_LIBRARY_PATH
+              export LIBGL_DRIVERS_PATH=$(nixGL printenv LIBGL_DRIVERS_PATH)
+              export __EGL_VENDOR_LIBRARY_FILENAMES=$(nixGL printenv __EGL_VENDOR_LIBRARY_FILENAMES)
+            else
+              echo "☢️ not running with nixGL"
+            fi
+
+            source ./install/setup.sh
+          '';
         };
       });
 
